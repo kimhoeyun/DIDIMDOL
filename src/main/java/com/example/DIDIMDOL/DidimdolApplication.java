5b87cdbe98d711867cd1a66a0496579a26022bde
kimhoeyun <clnvrd@suwon.ac.kr>
2025-09-29 12:02:31 +0900

    Initial commit

---
+++ src/main/java/com/example/DIDIMDOL/DidimdolApplication.java
@@ -0,0 +1,13 @@
+package com.example.DIDIMDOL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidimdolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidimdolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DIDIMDOL/TestDidimdolApplication.java
@@ -0,0 +1,11 @@
+package com.example.DIDIMDOL;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestDidimdolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(DidimdolApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
