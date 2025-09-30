350a5f2717c1222b865366e8f93fd42606b71a02
kimhoeyun <clnvrd@suwon.ac.kr>
2025-09-30 15:14:31 +0900

    chore: 패키지 위치 수정

--- src/test/java/com/DIDIMDOL/TestcontainersConfiguration.java
+++ src/test/java/com/DIDIMDOL/TestcontainersConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.DIDIMDOL;
+package com.DIDIMDOL;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
