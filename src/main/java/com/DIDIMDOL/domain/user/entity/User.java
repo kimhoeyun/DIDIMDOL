7c5ace34bf469ea6150727f7a0e1cd78a407c581
jeasangpark <renegade6@suwon.ac.kr>
2025-10-10 10:48:23 +0900

    refactor: User 엔티티 수정
    - Getter 어노테이션 추가
    - AllArgsConstructor 어노테이션 추가
    - 지정자 수정

--- src/main/java/com/DIDIMDOL/domain/user/entity/User.java
+++ src/main/java/com/DIDIMDOL/domain/user/entity/User.java
@@ -2,12 +2,17 @@ package com.DIDIMDOL.domain.user.entity;
 
 import com.DIDIMDOL.domain.progress.entity.Progress;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@AllArgsConstructor
+@Getter
 @NoArgsConstructor
+
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,7 +21,7 @@ public class User {
     private String email;
 
     @Column(nullable = false, unique = true)
-    private long userId;
+    private String userId;
 
     @Column
     private String password;
